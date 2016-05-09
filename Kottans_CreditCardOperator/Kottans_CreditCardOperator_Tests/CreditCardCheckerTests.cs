585104a693fe17938e8e222b65982f4579cb38e2
Yabko <o.yablinska@gmail.com>
2016-05-09 22:17:32 +0300

    added unittest project with a basic test

---
+++ Kottans_CreditCardOperator/Kottans_CreditCardOperator_Tests/CreditCardCheckerTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Kottans_CreditCardOperator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kottans_CreditCardOperator_Tests
+{
+    [TestClass]
+    public class CreditCardCheckerTests
+    {
+        [TestMethod]
+        public void IsCreditCardNumberValid_ValidNumber_Success()
+        {
+            CreditCardChecker ccchecker = new CreditCardChecker();
+            bool result = ccchecker.IsCreditCardNumberValid("341134113411347");
+            Assert.IsTrue(result);
+        }
+    }
+}

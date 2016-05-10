e70efa16b2a48d2640f3c08386914c53c6819d02
Olya <o.yablinska@gmail.com>
2016-05-10 13:12:18 +0300

    added basic unit-tests for GetCreditCardVendor

--- Kottans_CreditCardOperator/Kottans_CreditCardOperator_Tests/CreditCardCheckerTests.cs
+++ Kottans_CreditCardOperator/Kottans_CreditCardOperator_Tests/CreditCardCheckerTests.cs
@@ -56,5 +56,36 @@ namespace Kottans_CreditCardOperator_Tests
             bool result = ccchecker.IsCreditCardNumberValid("5105 1051 0510 5100");
             Assert.IsTrue(result);
         }
+        [TestMethod]
+        public void GetCreditCardVendor_AmExpress_15_Success()
+        {
+            CreditCardChecker ccchecker = new CreditCardChecker();
+            string result = ccchecker.GetCreditCardVendor("3411 3411 3411 347");
+            Assert.IsTrue(result=="American Express");
+        }
+
+        [TestMethod]
+        public void GetCreditCardVendor_JCB_16_Success()
+        {
+            CreditCardChecker ccchecker = new CreditCardChecker();
+            string result = ccchecker.GetCreditCardVendor("3566 0020 2036 0505");
+            Assert.IsTrue(result == "JCB");
+        }
+
+        [TestMethod]
+        public void GetCreditCardVendor_MasterCard_16_Success()
+        {
+            CreditCardChecker ccchecker = new CreditCardChecker();
+            string result = ccchecker.GetCreditCardVendor("5105 1051 0510 5100");
+            Assert.IsTrue(result == "MasterCard");
+        }
+
+        [TestMethod]
+        public void GetCreditCardVendor_Visa_13_Success()
+        {
+            CreditCardChecker ccchecker = new CreditCardChecker();
+            string result = ccchecker.GetCreditCardVendor("4222 2222 2222 2");
+            Assert.IsTrue(result == "VISA");
+        }
     }
 }
